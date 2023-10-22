61432aa8ced9b3498d9dac69a51d4be60edc3720
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-10-23 04:54:23 +0700

    fix (service): subscribe directly to the pose

--- movement/src/ThetaServer.cpp
+++ movement/src/ThetaServer.cpp
@@ -8,34 +8,40 @@
 class ThetaService : public rclcpp::Node {
 public:
   ThetaService() : Node("theta_service") {
-    pose_subscriber_ = std::make_shared<PoseSubscriber>();
+    subscription_ = this->create_subscription<turtlesim::msg::Pose>(
+        "turtle1/pose", 10, std::bind(&ThetaService::pose_callback, this, std::placeholders::_1));
     service_ = this->create_service<interfaces::srv::CalculateAngle>(
         "calculate_angle", std::bind(&ThetaService::callback, this, std::placeholders::_1, std::placeholders::_2));
   }
 
 private:
+  float x, y;
+
+  void pose_callback(const turtlesim::msg::Pose::SharedPtr pose) {
+    x = pose->x;
+    y = pose->y;
+  }
+
   void callback(const std::shared_ptr <interfaces::srv::CalculateAngle::Request> request,
                 std::shared_ptr <interfaces::srv::CalculateAngle::Response> response) {
-    float curr_x = pose_subscriber_->getX();
-    float curr_y = pose_subscriber_->getY();
     float goal_x = request->x;
     float goal_y = request->y;
 
     RCLCPP_INFO(this->get_logger(), "Received request with x: %f, y: %f", goal_x, goal_y);
-    RCLCPP_INFO(this->get_logger(), "Current position is x: %f, y: %f", curr_x, curr_y);
+    RCLCPP_INFO(this->get_logger(), "Current position is x: %f, y: %f", this->x, this->y);
 
     // handling for division by zero and third quadrant (returned in radian)
-    if (goal_x - curr_x == 0) {
-      if (goal_y - curr_y > 0) {
+    if (goal_x - this->x == 0) {
+      if (goal_y - this->y > 0) {
         response->theta = 1.5708;
       } else {
         response->theta = -1.5708;
       }
     } else {
-      response->theta = atan((goal_y - curr_y) / (goal_x - curr_x));
+      response->theta = atan((goal_y - this->y) / (goal_x - this->x));
     }
 
-    if (goal_x - curr_x < 0 && goal_y - curr_y < 0) {
+    if (goal_x - this->x < 0 && goal_y - this->y < 0) {
       response->theta += 3.14159;
     }
 
@@ -50,5 +56,5 @@ private:
   }
 
   rclcpp::Service<interfaces::srv::CalculateAngle>::SharedPtr service_;
-  std::shared_ptr <PoseSubscriber> pose_subscriber_;
+  rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscription_;
 };
